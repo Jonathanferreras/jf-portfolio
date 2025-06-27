ef98af9349826710df490bedaa153b209cd6f333
jonathanferreras <jonathanferreras@icloud.com>
2025-06-26 22:10:46 -0400

    updated type

--- src/services/work-experience/types/job.ts
+++ src/services/work-experience/types/job.ts
@@ -4,17 +4,17 @@ export type job = {
   description: string;
   industry: string;
   position: string;
-  startDate: string; // Format: MM/DD/YYYY
-  endDate: string; // Empty string if ongoing
+  startDate: string;
+  endDate: string;
   isFullTime: boolean;
   technologies: {
     frontend: string[];
     backend: string[];
     tools: string[];
   };
-  projects: []; // Can be replaced with a more specific type if project structure is known
-  achievements: []; // Same as above
-  logo: string; // Path to image
-  video: string; // URL or empty
-  link: string; // URL
+  projects: { name: string; description: string }[];
+  achievements: string[];
+  logo: string;
+  video: string;
+  link: string;
 };
